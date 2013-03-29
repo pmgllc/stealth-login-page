675443915ee7bf5b7091bc66f4c3f63035df19da
Jesse Petersen <jesse@petersenmediagroup.com>
2013-03-29 10:10:03 -0400

    a whole slew of PHP typos

--- includes/settings-page.php
+++ includes/settings-page.php
@@ -22,45 +22,22 @@ function slp_admin() {
 		
 		<?php settings_fields('slp_settings_group'); ?>
 
-		<div class="slp-page">
 		<p>Those attempting to gain access to your login form will be automatcally redirected to a customizble URL. Enter that URL below.</p>
-		<div class="clear">
-			<label class="description" for="slp_settings[redirect_url]"><?php _e('URL to redirect unauthorized attempts to:', 'slp_domain'); ?></label>
-			<input id="slp_settings[redirect_url]" name="slp_settings[redirect_url]" type="text" value="<?php echo $slp_settings['redirect_url']; ?>" />
-		</div>
+			<label class="description" for="<?php echo $slp_settings[redirect_url]; ?><?php _e('URL to redirect unauthorized attempts to', 'slp_domain'); ?>" ></label>
+			<input id="<?php echo $slp_settings[redirect_url]>" name="<?php echo $slp_settings[redirect_url]; ?>" type="text" value="<?php echo $slp_settings[redirect_url]; ?>" />
+
 		<p>The first part of the new URL string to reach your login form is the "question." It is just an arbitrary word or code. Complexity will not matter much at this time.</p>
-		<div class="clear">
-			<label class="description" for="<?php echo $slp_settings['question']; ?>"><?php _e('String used for the "question"', 'slp_domain'); ?></label>
-			<input type="text" name="<?php echo $slp_settings['question']; ?>" value="<?php echo $slp_settings['question']; ?>" />
-		</div>
+			<label class="description" for="<?php echo $slp_settings[question]; ?>"><?php _e('String used for the "question"', 'slp_domain'); ?></label>
+			<input type="text" name="<?php echo $slp_settings[question]; ?>" value="<?php echo $slp_settings[question]; ?>" />
+
 		<p>The second part of the new URL string to reach your login form is the "answer." It is also just an arbitrary word or code.</p>
-		<div class="clear">
-			<label class="description" for="<?php echo $slp_settings['answer']; ?>"><?php _e('String used for the "answer"', 'slp_domain'); ?></label>
-			<input type="text" name="<?php echo $slp_settings['answer']; ?>" value="<?php echo $slp_settings['answer']; ?>"  />
-		</div>
+			<label class="description" for="<?php echo $slp_settings[answer]; ?>"><?php _e('String used for the "answer"', 'slp_domain'); ?></label>
+			<input type="text" name="<?php echo $slp_settings[answer]; ?>" value="<?php echo $slp_settings[answer]; ?>"  />
+		
 		<p class="submit">
 			<input type="submit" class="button-primary" value="<?php _e('Save Settings', 'slp_domain'); ?>" />
 		</p>
-		</div>
 	</form>
-	<style type="text/css">
-	div.slp-page {
-		max-width: 600px;
-		min-width: 50%;
-	}
-	.slp-page div.clear {
-		clear: both;
-	}
-	.slp-page p {
-		font-size: 1.1em;
-		margin-bottom: 1em;
-		line-spacing: 1.4em;
-	}
-	.slp-page input[type="submit"],
-	.slp-page input[type="button"] {
-		margin-top: 1em;
-	}
-	</style>
 	</div>
 	<?php
 	echo ob_get_clean();
